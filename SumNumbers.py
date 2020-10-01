f89f25fa9af52c177b2cf6228e030a56b27479ad
hoummani <yassinehoummani.tsi@gmail.com>
2020-10-01 19:51:13 +0100

    the first exercise is done

---
+++ SumNumbers.py
@@ -0,0 +1,13 @@
+# #
+# Compute the sum of the first n positive integers
+#
+
+# Read input from the user
+n = int(input("Enter a positive integer :"))
+
+# Compute the sum
+sm = n * (n + 1) / 2
+
+# Display the result
+
+print("The sum of the first", n, "positive integers is :", sm)
